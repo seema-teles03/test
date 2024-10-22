4ee28af73e5eefdff84f05599ec890bd3e31d5df
Seema <seema.teles@logixal.com>
2024-10-22 10:52:26 +0530

    fix eslint issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,7 @@ export type State = {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `;
     } catch (error) {
+        console.log(error)
       // If a database error occurs, return a more specific error.
       return {
         message: 'Database Error: Failed to Create Invoice.',
@@ -99,7 +100,7 @@ export type State = {
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-    } catch (error) {
+    } catch (error) { console.log(error)
       return { message: 'Database Error: Failed to Update Invoice.' };
     }
    
@@ -108,12 +109,12 @@ export type State = {
   }
 
   export async function deleteInvoice(id: string) {
-    throw new Error('Failed to Delete Invoice');
+    //throw new Error('Failed to Delete Invoice');
     try {
       await sql`DELETE FROM invoices WHERE id = ${id}`;
       revalidatePath('/dashboard/invoices');
       return { message: 'Deleted Invoice.' };
-    } catch (error) {
+    } catch (error) { console.log(error)
       return { message: 'Database Error: Failed to Delete Invoice.' };
     }
   }
--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -19,7 +19,6 @@ const links = [
 ];
 
 export default function NavLinks() {
-  const pathname = usePathname();
   return (
     <>
       {links.map((link) => {
